8db9aa66d42ce2be3f0aa0e48f380a42340147d9
Your Name <zuhaib@mvp-apps.ae>
2025-10-28 22:36:07 +0400

    fix: set correct base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://zuhaib10.github.io',
+	base: '/zk-portfolio',
 	integrations: [tailwind()],
 });
